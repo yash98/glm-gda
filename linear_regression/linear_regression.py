890e8659e378c09db56b123849a04e6783f66b8f
yash98 <yashmalviya98@gmail.com>
2019-02-08 13:41:28 +0530

    add old work

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -4,6 +4,7 @@ import sys
 
 x = 0
 y = 0
+theta = 0
 
 def load_data (file_x, file_y):
     global x
@@ -12,11 +13,16 @@ def load_data (file_x, file_y):
     reader_y = csv.reader(open(file_y, "rt"), delimiter =',')
     list_x = list(reader_x)
     list_y = list(reader_y)
-    x = numpy.array(list_x).astype("float")
-    y = numpy.array(list_y).astype("float")
+    x = numpy.array(list_x, dtype=float)
+    y = numpy.array(list_y, dtype=float)
+
+def init():
+    global theta
+    load_data(sys.argv[1], sys.argv[2])
+    theta = numpy.zeros(shape=(x.size, 1), dtype=float)
 
 def main():
-    load_data(sys.argv[1], sys.argv[2])
+    init()
 
 if (__name__=="__main__"):
     main()
