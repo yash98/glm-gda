da0e4345d266047c3a897a04711a43f0918980af
yash98 <yashmalviya98@gmail.com>
2019-02-15 15:17:21 +0530

    linear reg animation first draft not working

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -3,7 +3,7 @@ import statistics
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
-from matplotlib import animation
+from matplotlib.animation import FuncAnimation
 
 x = 0
 y = 0
@@ -69,9 +69,12 @@ def batch_grad_desc (eta, epsilon):
     print(str(num_iterations) + ": " + str(lms_error))
             
 def plot_hypothesis():
-    plt.plot(x, y, marker='.', linestyle='None')
-    plt.plot([x_min, x_max], [theta[0]+theta[1]*xn_min, theta[0]+theta[1]*xn_max], marker='None')
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(x, y, marker='.', linestyle='None')
+    ax.plot([x_min, x_max], [theta[0]+theta[1]*xn_min, theta[0]+theta[1]*xn_max], marker='None')
     plt.show()
+    ax.clear()
 
 def mesh_of_error_function(t0, t1):
     tz = np.zeros(shape=(int(t0.size/t0[0].size), t0[0].size))
@@ -88,10 +91,18 @@ def error_function(t0s, t1s):
     z /= (2*y.size)
     return z
 
-# def animate_3d_theta_error(i):
+fig, ax = 0, 0
+theta_x = []
+theta_y = []
+theta_z = []
+ln = 0
+def mesh_init():
+    global ln, fig, ax
+    ax.set_title('3D mesh of error function')
+    ax.set_xlabel('Theta0')
+    ax.set_ylabel('Theta1')
+    ax.set_zlabel('J(Theta)')
 
-
-def plot_3d_error_mesh():
     t0first = theta_store[0][0]
     t0last = theta_store[len(theta_store)-1][0]
     t1first = theta_store[0][1]
@@ -100,19 +111,21 @@ def plot_3d_error_mesh():
     t1 = np.linspace(t1first, 2*t1last-t1first,50)
     T0, T1 = np.meshgrid(t0, t1)
     Z = mesh_of_error_function(T0, T1)
+    ln, = ax.plot_surface(T0, T1, Z, rstride=1, cstride=1, cmap='Reds', edgecolor='none', animated=True)
+    return ln,
 
-    # fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    ax.plot_surface(T0, T1, Z, rstride=1, cstride=1, cmap='Reds', edgecolor='none')
-    theta_x = [theta_store[i][0] for i in range(len(theta_store))]
-    theta_y = [theta_store[i][1] for i in range(len(theta_store))]
-    theta_z = [error_function(theta_x[i], theta_y[i]) for i in range(len(theta_store))]
-    ax.scatter3D(theta_x, theta_y, theta_z, c='black', marker='o')
-    
-    ax.set_title('3D mesh of error function')
-    ax.set_xlabel('Theta0')
-    ax.set_ylabel('Theta1')
-    ax.set_zlabel('J(Theta)')
+def mesh_update(frame):
+    global theta_x, theta_y, theta_z
+    theta_x.append(theta_store[frame][0])
+    theta_y.append(theta_store[frame][1])
+    theta_z.append(error_function(theta_x[frame], theta_y[frame]))
+    ax.clear()
+    ln, = ax.scatter3D(theta_x, theta_y, theta_z, c='black', marker='o', animated=True)
+    return ln,
+
+def plot_3d_error_mesh(time_gap):
+    fig, ax = plt.subplots()    
+    ani = FuncAnimation(fig, mesh_update, frames=range(len(theta_store)), init_func=mesh_init, interval=time_gap*1000)
     plt.show()
 
 def init():
@@ -125,9 +138,9 @@ def init():
 def main():
     init()
     normalize_stats_data()
-    batch_grad_desc(1.1, 0.000001)
-    plot_hypothesis()
-    plot_3d_error_mesh()
+    batch_grad_desc(float(sys.argv[3]), 0.00001)
+    # plot_hypothesis()
+    plot_3d_error_mesh(float(sys.argv[4]))
 
 if (__name__=="__main__"):
     main()
